532c90f0132af4ac1478c62c51aae736aac3cf59
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-04-03 23:39:19 +0530

    some theory about pandas

---
+++ Python_Libraries/pandas___.py
@@ -0,0 +1,42 @@
+'''
+📊 What is Pandas?
+
+Pandas is a powerful Python library used for:
+
+Data manipulation
+Data analysis
+Data cleaning
+
+It works mainly with structured data (tables like Excel).
+
+👉 Built on top of:
+
+NumPy (for fast calculations)
+❓ Why Pandas is Important
+1. Handle Large Data Easily
+Works with thousands to millions of rows
+Faster than normal Python lists
+2. Data Cleaning (Most Important in AI)
+Remove null values
+Fix wrong data
+Format data properly
+
+👉 In real life:
+
+70–80% work in AI/ML = Data Cleaning
+
+3. Easy Data Analysis
+Filtering
+Sorting
+Grouping
+Aggregation (sum, mean, etc.)
+4. Works with Real Data Files
+CSV
+Excel
+JSON
+SQL databases
+🧠 Core Data Structures in Pandas
+1. Series
+1D data (like a column)
+import pandas as pd
+data = pd.Series([10, 20, 30])'''
