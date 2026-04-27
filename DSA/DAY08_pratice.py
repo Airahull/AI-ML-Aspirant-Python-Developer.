716e91cdd3ed4aa667b5559dbaa9c9101a844cc4
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-04-27 23:57:53 +0530

    adding tower og hanoi

--- DSA/DAY08_pratice.py
+++ DSA/DAY08_pratice.py
@@ -382,7 +382,7 @@ index=0
 print(linear(lst,target,index))
     
 '''
-# tower of hanio very important recursive problem ...............................................................................................
+# tower of hanio very important recursive problem naturally recursive  ...............................................................................................
 def toh(n,scr,helper,destination):
     if n==1:
         print(f'from {scr} to {destination}')
