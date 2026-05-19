d2f1929fd31a536725c607fd00d936d159c336ea
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-05-19 21:43:40 +0530

    adding get request to server

---
+++ fastapi-project/basic-fastapi.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+# now creating a route in fastapi to get the data from the user and then we will use that data to predict the price of the product
+app = FastAPI()
+# it is a simple get request for checking api is working or not 
+# it is a static route (in which value doesnt change )
+@app.get("/") 
+def home():
+    return {"message ":"welcome to the ecomerce fastapi project"}
+
+#dynamic route is basically in which output change according to parameter 
+@app.get("/product/{id}")
+def getproduct(id :int):
+    products=["iphone","redmi","oppo","vivo"]
+    return products[id]
+
