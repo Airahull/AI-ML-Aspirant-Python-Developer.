c6a6444654f9273a5fa05812ea51aa88bd94d351
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-06-25 21:48:53 +0530

    two pointer question

--- DSA/day09_practice.py
+++ DSA/day09_practice.py
@@ -130,7 +130,6 @@ def trsum(lst,k):
 lst=[1,2,3,4,5,4,3,2,9]
 target = 12
 trsum(lst,target)           
-
 '''
 #recomended and best method imp'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 #using set
