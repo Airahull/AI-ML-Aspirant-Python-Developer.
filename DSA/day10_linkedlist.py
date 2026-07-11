d8ad6babdea64690e7afa863e78e01a7698f5a7c
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-07-11 21:48:31 +0530

    whole linked list revise

--- DSA/day10_linkedlist.py
+++ DSA/day10_linkedlist.py
@@ -221,7 +221,6 @@ print('mid element of your linked list is',ll.middle())
 print("-------------------------------------------")
 print("length of linked list is ",ll.length())
 print("-------------------------------------------")
-
 l2 =linkedlist()
 l2.insert_end(30)
 l2.insert_end(2)
