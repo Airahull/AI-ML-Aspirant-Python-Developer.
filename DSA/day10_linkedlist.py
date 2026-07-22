06d6f2aaeac9229fa8a29aca811705b0eafb3d56
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-07-22 21:10:46 +0530

    learn about notebook lm

--- DSA/day10_linkedlist.py
+++ DSA/day10_linkedlist.py
@@ -215,7 +215,6 @@ print("-------------------------------------------")
 print("head of linkedlist is :",ll.head.data)
 
 print("-------------------------------------------")
-
 print('mid element of your linked list is',ll.middle())
 print("-------------------------------------------")
 
