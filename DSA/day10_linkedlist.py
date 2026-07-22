a6cc2174cdea0c7e0a59ba720c47ecc0a1fb5d70
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-07-22 20:39:15 +0530

    adding some new question

--- DSA/day10_linkedlist.py
+++ DSA/day10_linkedlist.py
@@ -221,7 +221,6 @@ print("-------------------------------------------")
 
 print("length of linked list is ",ll.length())
 print("-------------------------------------------")
-
 #creating a object of linkedlist class for performing operation 
 l2 =linkedlist()
 l2.insert_end(30)
