77cbcf765cdae011e4798d4ba0008534d620ae24
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-07-23 22:19:20 +0530

    learning recursion function

--- DSA/day10_linkedlist.py
+++ DSA/day10_linkedlist.py
@@ -233,6 +233,7 @@ l2.insert_end(2)
 
 l2.display()
 l2.remove_dup_sorted() #  it not worked because of list is not sorted it only check adjusent element '''''''''
+
 l2.display()
 
 l2.remove_dup_unsorted()
