577c6c7ed007864d7199b4b048a0c512b4137c5b
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-07-24 21:15:16 +0530

    learning about how to prompt

--- DSA/day10_linkedlist.py
+++ DSA/day10_linkedlist.py
@@ -230,7 +230,6 @@ l2.insert_end(5)
 l2.insert_end(2)
 
 #for displaying linkedlist ..........
-
 l2.display()
 l2.remove_dup_sorted() #  it not worked because of list is not sorted it only check adjusent element '''''''''
 l2.display()
