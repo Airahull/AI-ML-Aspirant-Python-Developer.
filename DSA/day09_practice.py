9204b0296ff1498f4b67cd46e9469509eb705c07
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-07-27 23:33:46 +0530

    import a csv file

--- DSA/day09_practice.py
+++ DSA/day09_practice.py
@@ -45,7 +45,6 @@ text2=text.replace(" ","").lower()
 print(dict(Counter(text2)))
 print(Counter(text))
 print(type(text2))
-
 #Given two lists, find all the common elements between them.
 lst1=[1,2,34,4,5,6,7]
 lst2=[2,4,5,0,9,8,34]
