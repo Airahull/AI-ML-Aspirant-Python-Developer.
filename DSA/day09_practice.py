fa0e294794363aadf834ba133922a8d873df0e33
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-08-02 21:52:40 +0530

    optimize linkedin

--- DSA/day09_practice.py
+++ DSA/day09_practice.py
@@ -234,6 +234,7 @@ print(res.values())
 nd = { value:key for key ,value in dic.items()}
 print(nd)
 '''
+
 #Implement Bubble Sort to sort a list of numbers.
 '''
 n=int(input('enter the no of element in your list :' ))
