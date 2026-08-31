a1911ae8839cdb8f4c7361c8bec48a512ef59ccb
Rahul Rawat <rrahulrawatuk@gmail.com>
2026-08-31 21:13:49 +0530

    sql quearies

--- DSA/pdf-to-jpg.py
+++ DSA/pdf-to-jpg.py
@@ -5,7 +5,6 @@ pdf_path = r"C:\Users\lenovo\Documents\mcafile\PMC203_Assign_25020020573_Shailes
 output_folder = r"C:\Users\lenovo\Documents\mcafile"
 
 os.makedirs(output_folder, exist_ok=True)
-
 doc = fitz.open(pdf_path)
 
 for page_num in range(len(doc)):
@@ -13,6 +12,6 @@ for page_num in range(len(doc)):
     pix = page.get_pixmap(dpi=300)
     # This will save as 1.jpg, 2.jpg, 3.jpg inside mcafile
     pix.save(os.path.join(output_folder, f"{page_num + 1}.jpg"))
-    
+
 doc.close()
 print("Sab pages alag ho gaye! Check mcafile folder")
